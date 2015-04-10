524cab5167a3717458afe063a873ae2bfcbb1a2d
Tyler Ball <tyler@tylerball.net>
2015-04-10 00:46:20 -0400

    add photo content, fix photo display and urls

--- helpers/blog_image_helpers.rb
+++ helpers/blog_image_helpers.rb
@@ -11,7 +11,7 @@ module BlogImageHelpers
 
   def blog_post_images
     parent = Pathname.new(source_dir)
-    Dir.glob("#{File.join(source_dir, File.dirname(current_page.path))}/*.{jpg}").map do |p|
+    Dir.glob("#{File.join(source_dir, current_page.path.split('.')[0])}/*.{jpg}").map do |p|
       Photo.new((parent + p).relative_path_from(parent))
     end
   end
--- lib/photo.rb
+++ lib/photo.rb
@@ -1,11 +1,23 @@
 class Photo
-  attr_reader :path, :slug
+  attr_reader :path
 
   def initialize(path)
     @path = path
   end
 
+  def url
+    URI.escape(path.to_s)
+  end
+
   def slug
-    @path.basename.to_s.gsub(@path.extname, '')
+    path.basename.to_s.gsub(path.extname, '')
+  end
+
+  def name
+    /\d+-(.*)/.match(slug)[1]
+  end
+
+  def name_without_year
+    name.gsub(/\, \d+$/, '')
   end
 end
