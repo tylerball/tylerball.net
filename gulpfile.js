fc211e319e88b694abfe315e0223f743a21a755d
Tyler Ball <tyler@tylerball.net>
2016-08-29 15:27:25 -0400

    fix normalize minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')
 
 gulp.task('sass', function () {
-  return gulp.src('./source/stylesheets/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('./source/stylesheets/**/*.{scss,css}')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./.tmp/dist/stylesheets'));
 });
 
