e71aec8e0b743353489b51991371c61e93ea826f
Tyler Ball <tyler@tylerball.net>
2016-11-19 18:16:48 -0500

    update photos index style

--- lib/blog_image_helpers.rb
+++ lib/blog_image_helpers.rb
@@ -9,10 +9,10 @@ module BlogImageHelpers
         .first.to_s[%r{/blog/.+}].to_s)
   end
 
-  def blog_post_images
+  def blog_post_images(dir = current_page.path)
     source_dir = File.join(Middleman::Application.root, config[:source])
     parent = Pathname.new(source_dir)
-    Dir.glob("#{File.join(source_dir, current_page.path.split('.')[0])}/*.{jpg}").map do |p|
+    Dir.glob("#{File.join(source_dir, dir.split('.')[0])}/*.{jpg}").map do |p|
       Photo.new((parent + p).relative_path_from(parent))
     end
   end
