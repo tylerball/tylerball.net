2a4f52f3dd0af2334347f218722040845006dff5
Tyler Ball <tyler@tylerball.net>
2016-11-19 17:08:05 -0500

    add swiper carousel to photos pages

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,14 @@
-console.log('works');
+import Swiper from 'swiper';
+
+function init() {
+  new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 'auto',
+    paginationClickable: true,
+    keyboardControl: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+  });
+};
+
+document.addEventListener('DOMContentLoaded', init, false);
