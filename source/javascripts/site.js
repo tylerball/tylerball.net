9a37305f1c107c005f3281e00b531e7309d02e23
Tyler Ball <tyler@tylerball.net>
2018-06-01 00:19:16 -0400

    add simple lightbox to essays

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,5 @@
 import Swiper from 'swiper';
+const basicLightbox = require('basiclightbox');
 
 function init() {
   new Swiper('.swiper-container', {
@@ -12,3 +13,60 @@ function init() {
 };
 
 document.addEventListener('DOMContentLoaded', init, false);
+
+const lightbox = basicLightbox.create('<img>');
+let active;
+const images = Array.from(document.querySelectorAll('.lightbox img'));
+
+function activeImage() {
+  return lightbox.element().querySelector('img');
+}
+
+images.forEach((img, index) => {
+  const open = function () {
+    if (window.matchMedia("(max-width: 40em)").matches) {
+      return;
+    }
+    activeImage().src = '';
+    activeImage().src = img.src;
+    active = index;
+    lightbox.show();
+  }
+  img.addEventListener('click', open, false)
+});
+
+function prev() {
+  if (active > 0) {
+    activeImage().src = '';
+    activeImage().src = images[active - 1].src
+    active--;
+  }
+}
+
+function next() {
+  if (active < images.length - 1) {
+    activeImage().src = '';
+    activeImage().src = images[active + 1].src
+    active++;
+  }
+}
+
+function onKey(event) {
+  if (!lightbox.visible()) {
+    return;
+  }
+
+  switch (event.key) {
+    case 'Escape':
+      lightbox.close();
+      break;
+    case 'ArrowLeft':
+      prev();
+      break;
+    case 'ArrowRight':
+      next();
+      break;
+  }
+}
+
+document.addEventListener('keydown', onKey, false);
